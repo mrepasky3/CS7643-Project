5dbaa58889eb692f2ad72284f4e210b6cff6032c
mrepasky3 <mwrepasky@gmail.com>
2021-11-16 19:20:03 -0500

    Added loaders for toy, MNIST, and Omniglot tasks.

--- dataset_prep.py
+++ dataset_prep.py
@@ -2,6 +2,233 @@ import torch
 import torchvision
 import torchvision.datasets as datasets
 
+class SetsToy:
+	"""
+	A class to load various set classification tasks on sets of digits.
+	
+	...
+
+	Attributes
+	----------
+	train_data : tensor
+		60,000 random integers from 0 to 9
+	test_data : tensor
+		10,000 random integers from 0 to 9
+	set_bounds : tensor
+		random-size bounds on the indices for training sets
+	test_set_bounds : tensor
+		random-size bounds on the indices for testing sets
+
+	Methods
+	-------
+	sum_task():
+		Returns training and testing data and labels for sum of digits task.
+	max_task():
+		Returns training and testing data and labels for max of digits task.
+	range_task():
+		Returns training and testing data and labels for (max - min) of digits task.
+	mode_task():
+		Returns training and testing data and labels for (lowest-valued) mode of digits task.
+	product_task():
+		Returns training and testing data and labels for product of two largest digits task.
+	"""
+	def __init__(self,seed=None):
+		"""
+		Generates tensor of random integers and partitions into sets.
+
+		Parameters
+		----------
+			seed : int
+				random seed for reproducibility
+		"""
+		if seed is not None:
+			torch.manual_seed(seed)
+
+		self.train_data = torch.randint(10,(60000,))
+		self.test_data = torch.randint(10,(10000,))
+
+		# randomly determine the index range for each set
+		random_idx = torch.randint(2,10,(self.train_data.shape[0]//2,))
+		set_idx = torch.cumsum(random_idx,dim=0)
+		set_idx = set_idx[set_idx < self.train_data.shape[0]]
+
+		# keep all sets with multiple elements, store as tensor where each element is [lower,upper]
+		if (self.train_data.shape[0] - set_idx[-1]) > 1:
+		    self.set_bounds = torch.zeros((set_idx.shape[0]+1,2),dtype=torch.int)
+		    self.set_bounds[-1] = torch.tensor([set_idx[-1],self.train_data.shape[0]])
+		else:
+		    self.set_bounds = torch.zeros((set_idx.shape[0],2),dtype=torch.int)
+		self.set_bounds[0] = torch.tensor([0,set_idx[0]])
+		for i in range(set_idx.shape[0]-1):
+		    self.set_bounds[i+1] = torch.tensor([set_idx[i],set_idx[i+1]])
+
+		# randomly determine the index range for each test set
+		test_random_idx = torch.randint(2,10,(self.test_data.shape[0]//2,))
+		test_set_idx = torch.cumsum(test_random_idx,dim=0)
+		test_set_idx = test_set_idx[test_set_idx < self.test_data.shape[0]]
+
+		# keep all sets with multiple elements, store as tensor where each element is [lower,upper]
+		if (self.test_data.shape[0] - test_set_idx[-1]) > 1:
+		    self.test_set_bounds = torch.zeros((test_set_idx.shape[0]+1,2),dtype=torch.int)
+		    self.test_set_bounds[-1] = torch.tensor([test_set_idx[-1],self.test_data.shape[0]])
+		else:
+		    self.test_set_bounds = torch.zeros((test_set_idx.shape[0],2),dtype=torch.int)
+		self.test_set_bounds[0] = torch.tensor([0,test_set_idx[0]])
+		for i in range(test_set_idx.shape[0]-1):
+		    self.test_set_bounds[i+1] = torch.tensor([test_set_idx[i],test_set_idx[i+1]])
+
+	def sum_task(self):
+		"""
+		Returns training and testing data and labels for sum of digits task.
+
+		Returns
+		-------
+		train_sets : list
+			List of variable length training tensors of shape N where N is set size
+		train_labels : tensor
+			Contains integer sum of digits in training data
+		test_sets : list
+			List of variable length testing tensors of shape N where N is set size
+		test_labels : tensor
+			Contains integer sum of digits in testing data
+		"""
+		train_sets = []
+		train_labels = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.set_bounds.shape[0]):
+			train_sets.append(self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]])
+			train_labels[i] = (self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]]).sum()
+
+		test_sets = []
+		test_labels = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.test_set_bounds.shape[0]):
+			test_sets.append(self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
+			test_labels[i] = (self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]).sum()
+
+		return train_sets, train_labels, test_sets, test_labels
+
+	def max_task(self):
+		"""
+		Returns training and testing data and labels for max of digits task.
+
+		Returns
+		-------
+		train_sets : list
+			List of variable length training tensors of shape N where N is set size
+		train_labels : tensor
+			Contains integer max of digits in training data
+		test_sets : list
+			List of variable length testing tensors of shape N where N is set size
+		test_labels : tensor
+			Contains integer max of digits in testing data
+		"""
+		train_sets = []
+		train_labels = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.set_bounds.shape[0]):
+			train_sets.append(self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]])
+			train_labels[i] = (self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]]).max()
+
+		test_sets = []
+		test_labels = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.test_set_bounds.shape[0]):
+			test_sets.append(self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
+			test_labels[i] = (self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]).max()
+
+		return train_sets, train_labels, test_sets, test_labels
+
+	def range_task(self):
+		"""
+		Returns training and testing data and labels for (max - min) of digits task.
+
+		Returns
+		-------
+		train_sets : list
+			List of variable length training tensors of shape N where N is set size
+		train_labels : tensor
+			Contains integer (max - min) of digits in training data
+		test_sets : list
+			List of variable length testing tensors of shape N where N is set size
+		test_labels : tensor
+			Contains integer (max - min) of digits in testing data
+		"""
+		train_sets = []
+		train_labels = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.set_bounds.shape[0]):
+			train_sets.append(self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]])
+			temp_labels = self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]]
+			train_labels[i] = temp_labels.max() - temp_labels.min()
+
+		test_sets = []
+		test_labels = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.test_set_bounds.shape[0]):
+			test_sets.append(self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
+			temp_labels = self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]
+			test_labels[i] = temp_labels.max() - temp_labels.min()
+
+		return train_sets, train_labels, test_sets, test_labels
+
+	def mode_task(self):
+		"""
+		Returns training and testing data and labels for (lowest-valued) mode of digits task.
+
+		Returns
+		-------
+		train_sets : list
+			List of variable length training tensors of shape N where N is set size
+		train_labels : tensor
+			Contains lowest-valued integer mode of digits in training data
+		test_sets : list
+			List of variable length testing tensors of shape N where N is set size
+		test_labels : tensor
+			Contains lowest-valued integer mode of digits in testing data
+		"""
+		train_sets = []
+		train_labels = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.set_bounds.shape[0]):
+			train_sets.append(self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]])
+			temp_labels = self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]]
+			train_labels[i] = torch.bincount(temp_labels,minlength=10).argmax()
+
+		test_sets = []
+		test_labels = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.test_set_bounds.shape[0]):
+			test_sets.append(self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
+			temp_labels = self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]
+			test_labels[i] = torch.bincount(temp_labels,minlength=10).argmax()
+
+		return train_sets, train_labels, test_sets, test_labels
+
+	def product_task(self):
+		"""
+		Returns training and testing data and labels for product of two largest digits task.
+
+		Returns
+		-------
+		train_sets : list
+			List of variable length training tensors of shape N where N is set size
+		train_labels : tensor
+			Contains lowest-valued integer mode of digits in training data
+		test_sets : list
+			List of variable length testing tensors of shape N where N is set size
+		test_labels : tensor
+			Contains lowest-valued integer mode of digits in testing data
+		"""
+		train_sets = []
+		train_labels = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.set_bounds.shape[0]):
+			train_sets.append(self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]])
+			temp_labels = self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]]
+			train_labels[i] = temp_labels.sort().values[-2:].prod()
+
+		test_sets = []
+		test_labels = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.test_set_bounds.shape[0]):
+			test_sets.append(self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
+			temp_labels = self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]
+			test_labels[i] = temp_labels.sort().values[-2:].prod()
+
+		return train_sets, train_labels, test_sets, test_labels
+
+
 class SetsMNIST:
 	"""
 	A class to load various set classification tasks on MNIST.
@@ -39,6 +266,10 @@ class SetsMNIST:
 		Returns training and testing data and labels for max of digits task.
 	range_task():
 		Returns training and testing data and labels for (max - min) of digits task.
+	mode_task():
+		Returns training and testing data and labels for (lowest-valued) mode of digits task.
+	product_task():
+		Returns training and testing data and labels for product of two largest digits task.
 	"""
 	def __init__(self,seed=None):
 		"""
@@ -82,7 +313,7 @@ class SetsMNIST:
 		self.test_data_shuffled = self.test_data[test_shuffled_idx]
 		self.test_labels_shuffled = self.test_labels[test_shuffled_idx]
 
-		# randomly determine the index range for each set
+		# randomly determine the index range for each test set
 		test_random_idx = torch.randint(2,10,(self.test_data_shuffled.shape[0]//2,))
 		test_set_idx = torch.cumsum(test_random_idx,dim=0)
 		test_set_idx = test_set_idx[test_set_idx < self.test_data_shuffled.shape[0]]
@@ -192,9 +423,9 @@ class SetsMNIST:
 
 		return train_sets, train_labels, test_sets, test_labels
 
-	def range_task(self):
+	def mode_task(self):
 		"""
-		Returns training and testing data and labels for (max - min) of digits task.
+		Returns training and testing data and labels for (lowest-valued) mode of digits task.
 
 		Returns
 		-------
@@ -202,25 +433,173 @@ class SetsMNIST:
 			List of variable length training tensors of shape (N,D,D) where N is 
 			set size and D is image dimension
 		train_labels : tensor
-			Contains integer (max - min) of digits in training data
+			Contains lowest-valued integer mode of digits in training data
 		test_sets : list
 			List of variable length testing tensors of shape (N,D,D) where N is 
 			set size and D is image dimension
 		test_labels : tensor
-			Contains integer (max - min) of digits in testing data
+			Contains lowest-valued integer mode of digits in testing data
 		"""
 		train_sets = []
 		train_labels = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
 		for i in range(self.set_bounds.shape[0]):
 			train_sets.append(self.train_data_shuffled[self.set_bounds[i][0]:self.set_bounds[i][1]])
 			temp_labels = self.train_labels_shuffled[self.set_bounds[i][0]:self.set_bounds[i][1]]
-			train_labels[i] = temp_labels.max() - temp_labels.min()
+			train_labels[i] = torch.bincount(temp_labels,minlength=10).argmax()
 
 		test_sets = []
 		test_labels = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
 		for i in range(self.test_set_bounds.shape[0]):
 			test_sets.append(self.test_data_shuffled[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
 			temp_labels = self.test_labels_shuffled[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]
-			test_labels[i] = temp_labels.max() - temp_labels.min()
+			test_labels[i] = torch.bincount(temp_labels,minlength=10).argmax()
 
 		return train_sets, train_labels, test_sets, test_labels
+
+	def product_task(self):
+		"""
+		Returns training and testing data and labels for product of two largest digits task.
+
+		Returns
+		-------
+		train_sets : list
+			List of variable length training tensors of shape (N,D,D) where N is 
+			set size and D is image dimension
+		train_labels : tensor
+			Contains lowest-valued integer mode of digits in training data
+		test_sets : list
+			List of variable length testing tensors of shape (N,D,D) where N is 
+			set size and D is image dimension
+		test_labels : tensor
+			Contains lowest-valued integer mode of digits in testing data
+		"""
+		train_sets = []
+		train_labels = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.set_bounds.shape[0]):
+			train_sets.append(self.train_data_shuffled[self.set_bounds[i][0]:self.set_bounds[i][1]])
+			temp_labels = self.train_labels_shuffled[self.set_bounds[i][0]:self.set_bounds[i][1]]
+			train_labels[i] = temp_labels.sort().values[-2:].prod()
+
+		test_sets = []
+		test_labels = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.test_set_bounds.shape[0]):
+			test_sets.append(self.test_data_shuffled[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
+			temp_labels = self.test_labels_shuffled[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]
+			test_labels[i] = temp_labels.sort().values[-2:].prod()
+
+		return train_sets, train_labels, test_sets, test_labels
+
+
+class SetsOmniglot:
+	"""
+	A class to load unique character set classification task on Omniglot.
+	
+	...
+
+	Attributes
+	----------
+	train_data : tensor
+		training data loaded from Omniglot
+	train_labels : tensor
+		training labels loaded from Omniglot
+	test_data : tensor
+		testing data loaded from Omniglot
+	test_labels : tensor
+		testing labels loaded from Omniglot
+	set_bounds : tensor
+		random-size bounds on the indices for training sets
+	test_set_bounds : tensor
+		random-size bounds on the indices for testing sets
+
+	Methods
+	-------
+	unique_task():
+		Returns training and testing data and labels for unique symbols task.
+	"""
+	def __init__(self,seed=None):
+		"""
+		Loads the data from Omniglot, randomly shuffles, and generates set boundaries.
+
+		Parameters
+		----------
+			seed : int
+				random seed for reproducibility
+		"""
+		if seed is not None:
+			torch.manual_seed(seed)
+
+		omni_train = datasets.Omniglot(root='./data', background=True, download=True, transform=torchvision.transforms.ToTensor())
+		omni_test = datasets.Omniglot(root='./data', background=False, download=True, transform=torchvision.transforms.ToTensor())
+
+		data_loader = torch.utils.data.DataLoader(omni_train,
+	                                          batch_size=len(omni_train),
+	                                          shuffle=True)
+		self.train_data, self.train_labels = next(iter(data_loader))
+
+		data_loader = torch.utils.data.DataLoader(omni_test,
+	                                          batch_size=len(omni_test),
+	                                          shuffle=True)
+		self.test_data, self.test_labels = next(iter(data_loader))
+
+		# randomly determine the index range for each set
+		random_idx = torch.randint(20,50,(self.train_data.shape[0]//20,))
+		set_idx = torch.cumsum(random_idx,dim=0)
+		set_idx = set_idx[set_idx < self.train_data.shape[0]]
+
+		# keep all sets with multiple elements, store as tensor where each element is [lower,upper]
+		if (self.train_data.shape[0] - set_idx[-1]) > 1:
+		    self.set_bounds = torch.zeros((set_idx.shape[0]+1,2),dtype=torch.int)
+		    self.set_bounds[-1] = torch.tensor([set_idx[-1],self.train_data.shape[0]])
+		else:
+		    self.set_bounds = torch.zeros((set_idx.shape[0],2),dtype=torch.int)
+		self.set_bounds[0] = torch.tensor([0,set_idx[0]])
+		for i in range(set_idx.shape[0]-1):
+		    self.set_bounds[i+1] = torch.tensor([set_idx[i],set_idx[i+1]])
+
+		# randomly determine the index range for each test set
+		test_random_idx = torch.randint(20,50,(self.test_data.shape[0]//20,))
+		test_set_idx = torch.cumsum(test_random_idx,dim=0)
+		test_set_idx = test_set_idx[test_set_idx < self.test_data.shape[0]]
+
+		# keep all sets with multiple elements, store as tensor where each element is [lower,upper]
+		if (self.test_data.shape[0] - test_set_idx[-1]) > 1:
+		    self.test_set_bounds = torch.zeros((test_set_idx.shape[0]+1,2),dtype=torch.int)
+		    self.test_set_bounds[-1] = torch.tensor([test_set_idx[-1],self.test_data.shape[0]])
+		else:
+		    self.test_set_bounds = torch.zeros((test_set_idx.shape[0],2),dtype=torch.int)
+		self.test_set_bounds[0] = torch.tensor([0,test_set_idx[0]])
+		for i in range(test_set_idx.shape[0]-1):
+		    self.test_set_bounds[i+1] = torch.tensor([test_set_idx[i],test_set_idx[i+1]])
+
+	def unique_task(self):
+		"""
+		Returns training and testing data and labels for unique symbols task.
+
+		Returns
+		-------
+		train_sets : list
+			List of variable length training tensors of shape (N,D,D) where N is 
+			set size and D is image dimension
+		train_targets : tensor
+			Contains integer number of unique symbols in training sets
+		test_sets : list
+			List of variable length testing tensors of shape (N,D,D) where N is 
+			set size and D is image dimension
+		test_targets : tensor
+			Contains integer number of unique symbols in testing sets
+		"""
+		train_sets = []
+		train_targets = torch.zeros(self.set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.set_bounds.shape[0]):
+			train_sets.append(self.train_data[self.set_bounds[i][0]:self.set_bounds[i][1]])
+			temp_labels = self.train_labels[self.set_bounds[i][0]:self.set_bounds[i][1]]
+			train_targets[i] = len(temp_labels.unique())
+
+		test_sets = []
+		test_targets = torch.zeros(self.test_set_bounds.shape[0],dtype=torch.int)
+		for i in range(self.test_set_bounds.shape[0]):
+			test_sets.append(self.test_data[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]])
+			temp_labels = self.test_labels[self.test_set_bounds[i][0]:self.test_set_bounds[i][1]]
+			test_targets[i] = len(temp_labels.unique())
+
+		return train_sets, train_targets, test_sets, test_targets
