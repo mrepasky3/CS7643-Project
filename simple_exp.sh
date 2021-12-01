5053f00f14aec81c4b51ef76190d54f48201f533
Repasky <mrepasky3@isye-hps0005.isye.gatech.edu>
2021-12-01 10:03:59 -0500

    More structured experiments

---
+++ simple_exp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+python run_experiments.py --data_source "Toy" --experiment_type "simple" --task "sum" --max_size 20 --epochs 100
+python run_experiments.py --data_source "Toy" --experiment_type "simple" --task "max" --max_size 20 --epochs 100
+python run_experiments.py --data_source "Toy" --experiment_type "simple" --task "range" --max_size 20 --epochs 100
+python run_experiments.py --data_source "Toy" --experiment_type "simple" --task "mode" --max_size 20 --epochs 100
+python run_experiments.py --data_source "Toy" --experiment_type "simple" --task "product" --max_size 20 --epochs 100
+
+
+python run_experiments.py --data_source "MNIST" --experiment_type "simple" --task "sum" --max_size 20 --epochs 100
+python run_experiments.py --data_source "MNIST" --experiment_type "simple" --task "max" --max_size 20 --epochs 100
+python run_experiments.py --data_source "MNIST" --experiment_type "simple" --task "range" --max_size 20 --epochs 100
+python run_experiments.py --data_source "MNIST" --experiment_type "simple" --task "mode" --max_size 20 --epochs 100
+python run_experiments.py --data_source "MNIST" --experiment_type "simple" --task "product" --max_size 20 --epochs 100
+
+
+python run_experiments.py --data_source "OMNI" --experiment_type "simple" --task "unique" --max_size 30 --epochs 100 --batch_size 50
