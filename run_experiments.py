0d8a4ab174f900ba91e75cda034131e68208fe51
mrepasky3 <mwrepasky@gmail.com>
2021-12-04 07:51:55 -0500

    Added batch times experimenter

--- run_experiments.py
+++ run_experiments.py
@@ -25,7 +25,7 @@ parser.add_argument('--batch_size', type=int, default=200)
 parser.add_argument('--epochs', type=int, default=50)
 parser.add_argument('--seed', type=int, default=0)
 
-parser.add_argument('--experiment_type', type=str, choices=["simple","times"])
+parser.add_argument('--experiment_type', type=str, choices=["simple","times","batch_times"])
 parser.add_argument('--suppress_save', action="store_true")
 
 # simple training arguments
@@ -224,6 +224,58 @@ class Experimenter:
 				losses.append(loss.item())
 
 			times.append((arrow.now() - start_time).total_seconds())
+		return np.array(times).mean()
+
+
+	def report_batch_time(self, data, masks, outs, net):
+		"""
+		Choose a neural network to train on given data
+		for 5 epochs and return the average time of each batch.
+
+		Parameters
+		----------
+			data : torch.Tensor
+				tensor of training data
+			masks : torch.Tensor
+				indicators for non-present instances
+				in a set
+			outs : torch.Tensor
+				labels for the training data
+			net : str
+				choices are LSTM, DS, and ATT
+
+		Returns
+		-------
+			time: float
+				average duration in seconds of training
+				for a single epoch
+		"""
+		times = []
+		for _ in range(5):
+
+			bd, bm, bo = utils.batch_data(data,masks,outs,bs=self.batch_size)
+			losses = []
+			for i in tqdm(range(len(bd))):
+				start_time = arrow.now()
+				if net == "LSTM":
+					self.LSTM_optim.zero_grad()
+					loss = self.LSTM_network.compute_loss(bd[i],bm[i],bo[i])
+					loss.backward()
+					self.LSTM_optim.step()
+				elif net == "DS":
+					self.deepsets_optim.zero_grad()
+					loss = self.deepsets_network.compute_loss(bd[i],bm[i],bo[i])
+					loss.backward()
+					self.deepsets_optim.step()
+				elif net == "ATT":
+					self.attention_optim.zero_grad()
+					loss = self.attention_network.compute_loss(bd[i],bm[i],bo[i])
+					loss.backward()
+					self.attention_optim.step()
+
+				losses.append(loss.item())
+
+				times.append((arrow.now() - start_time).total_seconds())
 		return np.array(times).mean()
 
 
@@ -461,7 +513,7 @@ class Experimenter:
 			np.save(savepath+"/MSE_results.npy", MSE_results)
 
 
-	def time_experiments(self, task, fixed_sizes, savepath=None):
+	def time_experiments(self, task, fixed_sizes, savepath=None, time_type='epoch'):
 		"""
 		Trains the networks various fixed-sized sets for one
 		epoch and visualize the duration of one epoch for each
@@ -513,9 +565,14 @@ class Experimenter:
 			fixed_masks = fixed_masks.to(self.device)
 			fixed_outs = fixed_outs.to(self.device)
 
-			LSTM_times.append(self.report_train_time(fixed_data, fixed_masks, fixed_outs, net="LSTM"))
-			deepsets_times.append(self.report_train_time(fixed_data, fixed_masks, fixed_outs, net="DS"))
-			attention_times.append(self.report_train_time(fixed_data, fixed_masks, fixed_outs, net="ATT"))
+			if time_type == 'epoch':
+				LSTM_times.append(self.report_train_time(fixed_data, fixed_masks, fixed_outs, net="LSTM"))
+				deepsets_times.append(self.report_train_time(fixed_data, fixed_masks, fixed_outs, net="DS"))
+				attention_times.append(self.report_train_time(fixed_data, fixed_masks, fixed_outs, net="ATT"))
+			elif time_type == 'batch':
+				LSTM_times.append(self.report_batch_time(fixed_data, fixed_masks, fixed_outs, net="LSTM"))
+				deepsets_times.append(self.report_batch_time(fixed_data, fixed_masks, fixed_outs, net="DS"))
+				attention_times.append(self.report_batch_time(fixed_data, fixed_masks, fixed_outs, net="ATT"))
 
 			if self.device == "cuda:0":
 				del fixed_sets
@@ -535,8 +592,12 @@ class Experimenter:
 			plt.scatter(fixed_sizes, attention_times, marker='x', label="Self Attention")
 			plt.legend(fontsize=14)
 			plt.xlabel('Set Size',fontsize=14)
-			plt.ylabel('Train Time',fontsize=14)
-			plt.savefig(savepath+'/train_time_curves.png')
+			if time_type == 'epoch':
+				plt.ylabel('Train Time',fontsize=14)
+				plt.savefig(savepath+'/train_time_curves.png')
+			elif time_type == 'epoch':
+				plt.ylabel('Batch Train Time',fontsize=14)
+				plt.savefig(savepath+'/batch_time_curves.png')
 			plt.clf()
 
 
@@ -576,5 +637,15 @@ if __name__ == "__main__":
 			exp.time_experiments(args.task, fixed_sizes=size_list)
 		else:
 			exp.time_experiments(args.task, fixed_sizes=size_list, savepath=args.data_source+"_"+args.task)
+
+	elif args.experiment_type == 'batch_times':
+		if args.data_source == "OMNI":
+			size_list = np.arange(21,40)
+		else:
+			size_list = np.arange(2,60)
+		if args.suppress_save:
+			exp.time_experiments(args.task, fixed_sizes=size_list, time_type='batch')
+		else:
+			exp.time_experiments(args.task, fixed_sizes=size_list, savepath=args.data_source+"_"+args.task, time_type='batch')
 
 	print('COMPLETED WITHOUT MAJOR ERROR')
